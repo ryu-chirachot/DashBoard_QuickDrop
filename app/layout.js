1059d11b2f8e666dc3955a9439b43cfcf0694d1a
chirachot <144918263+ryu-chirachot@users.noreply.github.com>
2025-03-10 22:00:42 +0700

    TestDeploy

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import "@/app/globals.css"
 import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -12,12 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="th" suppressHydrationWarning>
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }
-
